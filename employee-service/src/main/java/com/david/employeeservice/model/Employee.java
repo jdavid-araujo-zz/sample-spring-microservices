2000ed1ac330741cd7f34227b564ca7dfb4f6502
jdavid-araujo <j.davidsousaaraujo@gmail.com>
2019-07-29 13:04:16 -0300

    Add configuration by git and Eureka

--- employee-service/src/main/java/com/david/employeeservice/model/Employee.java
+++ employee-service/src/main/java/com/david/employeeservice/model/Employee.java
@@ -16,5 +16,60 @@ public class Employee {
 	
 	private String name;
 	
+	private String phone;
+	
 	private Integer age;
+	
+	private Long departmentId;
+
+	public Employee() {}
+
+	public Employee(Long id, String name, String phone, Integer age, Long departmentId) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+		this.age = age;
+		this.departmentId = departmentId;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
 }
