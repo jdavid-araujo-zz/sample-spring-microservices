144b66a718c3495135c2bff9e86dfa3ab54f3ba1
jdavid-araujo <j.davidsousaaraujo@gmail.com>
2019-07-29 13:15:11 -0300

    Ajust of null point

--- employee-service/src/main/java/com/david/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/david/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -1,18 +1,25 @@
 package com.david.employeeservice.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.david.employeeservice.model.Employee;
+import com.david.employeeservice.repository.EmployeeRepository;
 import com.david.employeeservice.service.EmployeeService;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-	private EmployeeService employeeService;
+	private EmployeeRepository employeeRepository;
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
+	}
 	
 	@Override
 	public void save(Employee entity) {
-		this.employeeService.save(entity);
+		this.employeeRepository.save(entity);
 	}
 
 	@Override
@@ -29,13 +36,12 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 	@Override
 	public Iterable<Employee> findAll() {
-		return this.employeeService.findAll();
+		return this.employeeRepository.findAll();
 	}
 
 	@Override
 	public void deleteById(Long id) {
-		// TODO Auto-generated method stub
-		
+		this.employeeRepository.deleteById(id);
 	}
 
 }
